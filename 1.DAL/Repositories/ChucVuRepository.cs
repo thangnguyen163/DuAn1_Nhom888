7d0e0262db355d60476912d525d972d81fe879b5
Loi3107 <loitqph26025@fpt.edu.vn>
2022-11-10 18:18:32 +0700

    sad

--- 1.DAL/Repositories/ChucVuRepository.cs
+++ 1.DAL/Repositories/ChucVuRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _1.DAL.Context;
+using _1.DAL.DomainClass;
+using _1.DAL.IRepositoties;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,41 @@ using System.Threading.Tasks;
 
 namespace _1.DAL.Repositoties
 {
-    public class ChucVuRepository
+    public class ChucVuRepository : IChucVuRepository
     {
+        private FpolyDBContext _DBcontext;
+        private List<ChucVu> _lstChucVu;
+        public ChucVuRepository()
+        {
+            _DBcontext = new FpolyDBContext();
+            _lstChucVu = new List<ChucVu>();    
+        }
+
+        public bool addChucVu(ChucVu chucVu)
+        {
+            _DBcontext.ChucVus.Add(chucVu);
+            _DBcontext.SaveChanges();
+            return true;
+        }
+
+        public bool deleteChucVu(ChucVu chucVu)
+        {
+            _DBcontext.ChucVus.Remove(chucVu);
+            _DBcontext.SaveChanges();
+            return true;
+        }
+
+        public List<ChucVu> getChucVusFromDB()
+        {
+            _lstChucVu = _DBcontext.ChucVus.ToList();
+            return _lstChucVu;
+        }
+
+        public bool updateChucVu(ChucVu chucVu)
+        {
+            _DBcontext.ChucVus.Update(chucVu);
+            _DBcontext.SaveChanges();
+            return true;
+        }
     }
 }
